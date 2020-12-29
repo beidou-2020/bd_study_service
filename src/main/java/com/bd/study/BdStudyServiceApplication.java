158a436eb5ada7dbc38c9279c63fb9da25feb8cc
beidou <18513032646@163.com>
2020-12-29 22:26:41 +0800

    部署：本地生产环境隔离consul监控

--- src/main/java/com/bd/study/BdStudyServiceApplication.java
+++ src/main/java/com/bd/study/BdStudyServiceApplication.java
@@ -3,14 +3,13 @@ package com.bd.study;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"com.bd.study"})
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @MapperScan("com.bd.study.repository")
 @EnableTransactionManagement    //开启事务注解
-@EnableDiscoveryClient          //开启服务发现(consul)
+// @EnableDiscoveryClient          //开启服务发现(consul)
 public class BdStudyServiceApplication {
 
     public static void main(String[] args) {
